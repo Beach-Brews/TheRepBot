bc5d4f818648532f8eeffc81e324d2b159fd17a9
beach-brews <198412879+beach-brews@users.noreply.github.com>
2026-03-14 21:39:19 -0400

    (WIP) Initial folder structure and configs complete. Need to work on server -> client interactions.

---
+++ src/client/Leaderboard.tsx
@@ -0,0 +1,18 @@
+import './index.css';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+export const Leaderboard = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Leaderboard />
+  </StrictMode>
+);
